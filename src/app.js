9581c1b6c15940253a166ce1e6b1659502840396
ingu <inanc.guerkan@netlight.com>
2022-05-19 23:26:26 +0200

    javascript falan on hoverlar

---
+++ src/app.js
@@ -0,0 +1,21 @@
+document.getElementById("voicemail").style.filter = "blur(5px)";
+
+function tarih_yaz1() {
+  const element = document.getElementById("1");
+  element.src="blurlu1.png"
+}
+
+function tarih_yaz2() {
+  const element = document.getElementById("2");
+  element.src="blurlu2.png"
+}
+
+function tarih_yaz3() {
+  const element = document.getElementById("3");
+  element.src="blurlu3.png"
+}
+
+function tarih_yaz4() {
+  const element = document.getElementById("4");
+  element.src="blurlu4.png"
+}
