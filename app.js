94d16be9e9adc0a36832ce871e9708fc2a65f290
ingu <inanc.guerkan@netlight.com>
2022-05-20 17:32:07 +0200

    baya temizlik + sicak sohbet gif

---
+++ app.js
@@ -0,0 +1,8 @@
+function show_blurred_date_1() {
+  const element = document.getElementById("1");
+  element.src="img/blurlu1.png"
+
+  const audio_elem = document.getElementById("audio_elem")
+  audio_elem.setAttribute("src","sounds/ses1.mp3");
+  audio_elem.setAttribute("autoplay", "true")
+}
